d8b89c3a58c8913f3335b457991b50e9be174a81
Alexander Nollet <alexanderacj13@gmail.com>
2025-08-02 17:50:31 +0200

    endangered stations start flickering

--- lib/station/station.gd
+++ lib/station/station.gd
@@ -1,23 +1,20 @@
 extends Area2D
 class_name Station
 
-var timer_scene: PackedScene = preload("uid://d10fj4d10l5ak")
+@export var color: Color = Color.TRANSPARENT
 
 ## When empty there are no passengers at this station
 var passengers: Passengers:
 	set(new_passengers):
 		passengers = new_passengers
-		if new_passengers:
-			var station_progress: StationProgress = timer_scene.instantiate()
-			station_progress.passengers = new_passengers
-			station_progress.station = self
-			%PassengersControl.add_child(station_progress)
-		else:
-			for child in %PassengersControl.get_children():
-				child.queue_free()
+		%StationProgress.passengers = new_passengers
 
 ## Null when no train is in the station.
 var train: Train
+
+func _ready() -> void:
+	%StationProgress.station = self
+	%StationProgress.color = color
 
 func _process(delta: float) -> void:
 	if train and train.speed == 0: # If the train has stopped we can board the passengers.
@@ -36,4 +33,3 @@ func _on_area_exited(_area: Area2D) -> void:
 
 func _on_area_entered(area:Area2D) -> void:
 	train = area.get_parent() # We know this will be train because of the collision layer.
-
--- lib/station/station_progress.gd
+++ lib/station/station_progress.gd
@@ -1,7 +1,22 @@
 extends Control
 class_name StationProgress
 
-var passengers: Passengers
+## The tween that controls the scale of this progress.
+var tween: Tween
+
+var passengers: Passengers:
+	set(new_passengers):
+		passengers = new_passengers
+
+		if passengers:
+			%AnimationPlayer.play("bounce")
+		else:
+			%AnimationPlayer.play("RESET")
+
+var color: Color = Color.TRANSPARENT:
+	set(new_color):
+		%Progress.tint_progress = new_color
+		%StationName.label_settings.font_color = new_color
 
 var station: Station:
 	set(new_station):
@@ -11,3 +26,11 @@ var station: Station:
 func _process(_delta: float) -> void:
 	if passengers:
 		%Progress.value = passengers.patience / passengers.max_patience * 100
+		if %Progress.value <= 25:
+			%AnimationPlayer.speed_scale = 2
+		elif %Progress.value <= 50:
+			%AnimationPlayer.speed_scale = 1.5
+		
+	else:
+		%AnimationPlayer.speed_scale = 1
+		%Progress.value = 100
