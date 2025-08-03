eb46d69130ccaf4e08ff67eee6af86c1ec0ad26d
Alexander Nollet <alexanderacj13@gmail.com>
2025-08-03 11:33:18 +0200

    fullscreen button

---
+++ lib/ui/main/full_screen_button.gd
@@ -0,0 +1,25 @@
+extends Control
+class_name FullScreenButton
+
+@export var fullscreen_texture: Texture
+@export var minimized_texture: Texture
+
+func _ready() -> void:
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		maximized = true
+	else:
+		maximized = false
+
+var maximized: bool:
+	set(new_maximized):
+		maximized = new_maximized
+
+		if maximized:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			%Button.icon = minimized_texture
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			%Button.icon = fullscreen_texture
+
+func _on_button_pressed() -> void:
+	maximized = !maximized
